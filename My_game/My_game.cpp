8f0427b28b0bfd905bb966253821b071325fb694
HuKuTa-y <zelleonid73@gmail.com>
2025-09-14 18:52:30 +0300

    Update My_game.cpp

--- My_game/My_game.cpp
+++ My_game/My_game.cpp
@@ -33,36 +33,74 @@ void MoveRectangle(Rectangle& rec, bool controls) {
 int main(void)
 {
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
     SetTargetFPS(60);
 
+    
     Image image = LoadImage("cat.png");
+    Image image2 = LoadImage("dog.png");
     ImageResize(&image, 132, 114);
+    ImageResize(&image2, 132, 114);
     Texture2D texture = LoadTextureFromImage(image);
+    Texture2D texture2 = LoadTextureFromImage(image2);
     UnloadImage(image);
+    UnloadImage(image2);
 
-    Rectangle rec = { screenWidth / 2 - 50, screenHeight / 2 - 50,100,100 };
-    Rectangle rec2 = { screenWidth / 4 - 50, screenHeight / 4 - 50,100,100 };
+   
+    Rectangle rec = { screenWidth / 2 - 50, screenHeight / 2 - 50, 100, 100 };
+    Rectangle rec2 = { screenWidth / 4 - 50, screenHeight / 4 - 50, 100, 100 };
+
+    
+    Color backgroundColor = RAYWHITE;
+
+    
+    Rectangle buttonRec = { 700, 50, 80, 45 }; 
+
     while (!WindowShouldClose())
     {
+        
+        Vector2 mousePoint = GetMousePosition();
+
+        
+        if (CheckCollisionPointRec(mousePoint, buttonRec) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+        {
+            
+            if (backgroundColor.r == RAYWHITE.r && backgroundColor.g == RAYWHITE.g && backgroundColor.b == RAYWHITE.b)
+            {
+                backgroundColor = DARKGRAY; 
+            }
+            else
+            {
+                backgroundColor = RAYWHITE; 
+            }
+        }
+
+        
         MoveRectangle(rec, false);
         MoveRectangle(rec2, true);
 
+        
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(backgroundColor);
 
+       
+        DrawRectangleRec(buttonRec, DARKGRAY);
+        DrawRectangleRec(buttonRec, BLACK); 
+        DrawText("Night", buttonRec.x + 10, buttonRec.y + 10, 23, WHITE);
 
+        
         DrawRectangleRec(rec, BLUE);
         DrawRectangleRec(rec2, RED);
         DrawText("Bob", rec.x, rec.y, 20, BLACK);
         DrawTexture(texture, rec.x, rec.y, WHITE);
-
+        DrawTexture(texture2, rec2.x, rec2.y, WHITE);
         DrawText("John", rec2.x, rec2.y, 20, BLACK);
+
         EndDrawing();
-
     }
-    UnloadTexture(texture);
 
+    
+    UnloadTexture(texture);
+    UnloadTexture(texture2);
     CloseWindow();
 
     return 0;
